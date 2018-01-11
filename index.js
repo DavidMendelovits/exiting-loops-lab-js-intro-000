452831f08d690fe102827491fe01a43ac0ff5893
David Mendelovits <david.mendelovits@gmail.com>
2018-01-11 23:47:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function breakOut(array, changeValue, stopValue) {
   for(i = 0; i >= array.length; i++) {
-    if(array === stopValue) {
+    if(array[i] === stopValue) {
       break;
     }
     changeValue(array[i])
